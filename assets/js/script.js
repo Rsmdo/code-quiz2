dd3ecfd6c9cb78885afbffeca6897c4e3224a00f
Royden Semedo <roydensemedo@Roydens-MacBook-Pro.local>
2022-07-13 11:51:43 -0400

    added questions array

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+// array to define questions
+// Note: correct answer counts from 0-3
+const questions = [
+    {
+        question: 'Commonly used data types DO NOT include:',
+        answers: [
+            'strings', 
+            'booleans',
+            'numbers',
+            'alerts',
+
+        ],
+        correct: 3,
+    },
+    {
+        question: 'The condition in an if / else statement is enclosed within ?',
+        answers: [
+            'parentheses',
+            'curly', 
+            'square brackets',
+            'quotes',
+        ],
+        correct: 0,
+    },
+    {
+        question: 'Arrays in JavaScript can be used to store ?',
+        answers: [
+            'Answer',
+            'other arrays',
+            'booleans',
+            'All of the above',
+        ],
+        correct: 3,
+    },
+    {
+        question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+        answers: [
+            'Javascript',
+            'console log',
+            'terminal/bash',
+            'for loops',
+        ],
+        correct: 1,
+    }
+]
